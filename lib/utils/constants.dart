95f8e89f837747a8f7ec082fa9c7a3b8c53d8978
Vijay Nadkarni <vjnadkarni@gmail.com>
2025-10-09 22:30:24 -0700

    WIP: Sync from Vijays-Mac-Mini-M4-2024 at 2025-10-09 22:30:24
    Auto-commit of work in progress files

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -23,6 +23,14 @@ class AppConstants {
   static String get conversationCheckEndpoint => '$backendUrl/conversation/check';
   static String get conversationClearEndpoint => '$backendUrl/conversation/clear';
 
+  // Mobile Avatar WebView URL
+  static String get mobileAvatarUrl {
+    // In development, use local network
+    // In production, this will be https://coach.galenogen.com/mobile-avatar
+    final host = backendUrl.replaceAll(':8000', ':3000'); // Web app runs on port 3000
+    return '$host/mobile-avatar';
+  }
+
   // Colors
   static const primaryColor = 0xFF9C27B0; // Purple
   static const secondaryColor = 0xFF2196F3; // Blue
